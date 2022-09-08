c7e31e3a4ee76a7e338cfed0eda05287d9acbdd3
jkresak101 <jkresak101@gmail.com>
2022-09-08 14:27:46 -0400

    update on code, bools and fools.

---
+++ Fall2022-Conditionals/Fall2022-Conditionals/Program.cs
@@ -0,0 +1,16 @@
+﻿var isSunny = true;
+var isRaining = false;
+var isCold = false;
+var temp = 75;
+if(isSunny && temp <= 65 +12)
+{
+    Console.WriteLine("It's Sunny Dummy!");
+        }
+else if(isSunny && temp >=65)
+{
+    Console.WriteLine("it's stinky and sweet");
+}
+else
+{
+    Console.WriteLine("It's Not, Its Dark and Scary :(");
+}
