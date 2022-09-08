d587b72606591f451509ae3a0c78e98afe957972
jkresak101 <jkresak101@gmail.com>
2022-09-08 15:15:10 -0400

    updated stuff for class

--- Fall2022-Conditionals/Fall2022-Conditionals/Program.cs
+++ Fall2022-Conditionals/Fall2022-Conditionals/Program.cs
@@ -2,7 +2,7 @@
 var isRaining = false;
 var isCold = false;
 var temp = 75;
-if(isSunny && temp <= 65 +12)
+if(isSunny && temp < 65)
 {
     Console.WriteLine("It's Sunny Dummy!");
         }
@@ -14,3 +14,31 @@ else
 {
     Console.WriteLine("It's Not, Its Dark and Scary :(");
 }
+
+var errorcode = 404;
+switch (errorcode)
+{
+    case 404:
+        Console.WriteLine("Page Not Found");
+        break;
+    default:
+        Console.WriteLine("Unknown error occured");
+        break;
+}
+
+var moodRating = 0;
+var isHappy = false;
+var isGlas = true;
+if(isHappy)
+{
+    moodRating = 100;
+}
+else
+{
+    moodRating = 0;
+}
+// same code as the lines above
+moodRating = isHappy ? 100 : -100;
+
+moodRating = moodRating + 1;
+moodRating += 1;
